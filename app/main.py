679f9a23ca69a2af7c6acc543960a970c9a790fa
serhii93git <s.g.demchenko93@gmail.com>
2024-06-30 22:18:40 +0200

    add User model. set up message as json

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, WebSocket, Depends
+import json  # Import json module
+from fastapi import FastAPI, WebSocket, Depends, Query
 from typing import List
 
-from sqlmodel import SQLModel, Session
-from .database import engine, get_session
+from sqlmodel import SQLModel, Session, select
 
-from .models import Message
+from .database import engine, get_session
+from .models import Message, User
 
 app = FastAPI()
 
@@ -30,24 +31,41 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
+
+
 @app.on_event('startup')
 def on_startup():
     create_db_and_tables()
 
+
 @app.websocket("/ws/chat")
-async def websocket_endpoint(websocket: WebSocket, session: Session = Depends(get_session)):
+async def websocket_endpoint(websocket: WebSocket, username: str = Query(...), session: Session = Depends(get_session)):
     await manager.connect(websocket)
+
+    user = session.exec(select(User).where(User.username == username)).first()
+    if not user:
+        user = User(username=username)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
     try:
         while True:
             data = await websocket.receive_text()
-            message = Message(content=data)
+            message = Message(content=data, user_id=user.id)
             session.add(message)
             try:
                 session.commit()
             except Exception as e:
                 session.rollback()
                 print(f"Error committing message: {e}")
-            await manager.broadcast(data)
+
+            # Create a JSON message
+            json_message = {
+                "username": username,
+                "content": data
+            }
+            await manager.broadcast(json.dumps(json_message))
     except Exception as e:
         print(f"Error: {e}")
     finally:
--- app/models.py
+++ app/models.py
@@ -1,7 +1,20 @@
-from sqlmodel import SQLModel, Field
+from typing import List, Optional
+from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime
+
 
 class Message(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     content: str = Field(title='повідомлення')
     send_time: datetime = Field(default_factory=datetime.utcnow)
+    user_id: int = Field(foreign_key="user.id")
+    user: Optional['User'] = Relationship(back_populates="messages")
+
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(title='нік')
+    messages: List[Message] = Relationship(back_populates="user")
+
+
+Message.user = Relationship(back_populates="messages")
